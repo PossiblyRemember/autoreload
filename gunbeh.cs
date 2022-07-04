66624bc4976ee4698518ad9febb67a4907325cf3
BuildTools <unconfigured@null.spigotmc.org>
2022-07-04 02:24:02 -0400

    I did a LOT, no errors now, now I just have to test it in-game

--- gunbeh.cs
+++ gunbeh.cs
@@ -8,21 +8,28 @@ public class Gunbeh : FirearmBehaviour
     public bool defaultBulletsb = true;
     private int DefaultBullets = 6;
     private int Bullets = 0;
-    private object Gun = Get
+    [SerializeField] private float _time = 7f;
+
     void Update()
     {
-        if(defaultBulletsb == true)
+        if (defaultBulletsb == true)
         {
             Bullets = DefaultBullets;
         }
-        Gun.FirearmBehaviour.OnFire
+        GetComponent<UseEventTrigger>().Action = () =>
         {
-            Bullets -- Bullets - 1;
-        }
+            Bullets--;
+            ModAPI.Notify("You have " + Bullets + "left!");
+        };
         if (default == 0)
         {
-            FireDelay = 7;
-            DefaultBullets = 
+            this.BulletsPerShot = 0;
+            Invoke("ResetBullet", _time);
         }
+    }
+    public void ResetBullet()
+    {
+        Bullets = DefaultBullets;
+        this.BulletsPerShot = 0;
     }
 }
